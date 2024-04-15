b857e34e384ff548f11dc2426bd31c45782d577f
miapolidore <156038306+miapolidore@users.noreply.github.com>
2024-04-15 17:57:37 -0500

    step 2 - Progress

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-git add .
+import mysql.connector # type: ignore
 
+cnx = mysql.connector.connect(user="rohan", password="pass1234", host="localhost")
+print(cnx)
+
+'''
+connection = mysql.connector.connect(user = ' ', database = ' ', password = ' ')
+cursor = connection.cursor()
+testQuery = ("SELECT * FROM "put something here idk")
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+cursor.close()
+connection.close()
+'''
